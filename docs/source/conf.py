94cdf29b2fa143aef594f9a97e7f179af344307c
Hap <hapleondon@gmail.com>
2024-07-02 19:19:12 +0200

    Changed copyright stuff on conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RTD Tutorial'
+copyright = '2024, Hap'
+author = 'Hap'
 
 release = '0.1'
 version = '0.1.0'
